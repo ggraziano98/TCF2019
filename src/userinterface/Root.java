e93a29eab0160848aebbde8ac28b45dfbbaef7ad
davidedionese <davide.dionese@hotmail.it>
2019-11-28 12:47:50 +0100

    bottoni azionati con tasti

--- src/userinterface/Root.java
+++ src/userinterface/Root.java
@@ -6,15 +6,19 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.VPos;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.ColumnConstraints;
@@ -24,6 +28,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -69,66 +74,80 @@ public class Root extends Application {
 
 
 		HBox findHBox = new HBox();
+		findHBox.setAlignment(Pos.CENTER);
 		TextField findText = new TextField();
 		findText.setMinWidth(250);
-		Label cerca = new Label("cerca...");
 		Button findButton = new Button("cerca");
+		findButton.setOnMouseClicked((e) -> {
+			System.out.println(findText.getText());
+		});
+		findHBox.setOnKeyReleased((final KeyEvent KeyEvent) -> {
+			if (KeyEvent.getCode() == KeyCode.ENTER) System.out.println(findText.getText());
+		});
 		findHBox.getChildren().add(findText);	
 		findHBox.getChildren().add(findButton);
+		
 
 
 
-		FileInputStream img = new FileInputStream("files\\Player\\play.png");
-		Image image = new Image(img);
-		ImageView Img = new ImageView(image);
-		Img.setFitHeight(25);
-		Img.setFitWidth(25);
+		FileInputStream playFile = new FileInputStream("files\\Player\\play.png");
+		Image playImage = new Image(playFile);
+		ImageView playView = new ImageView(playImage);
+		playView.setFitHeight(25);
+		playView.setFitWidth(25);
 
-		FileInputStream paimg = new FileInputStream("files\\Player\\pause.png");
-		Image paimage = new Image(paimg);
-		ImageView paImg = new ImageView(paimage);
-		paImg.setFitHeight(25);
-		paImg.setFitWidth(25);
+		FileInputStream pauseFile = new FileInputStream("files\\Player\\pause.png");
+		Image pauseImage = new Image(pauseFile);
+		ImageView pauseView = new ImageView(pauseImage);
+		pauseView.setFitHeight(25);
+		pauseView.setFitWidth(25);
 
-		FileInputStream pimg = new FileInputStream("files\\Player\\ps.png");
-		Image pimage = new Image(pimg);
-		ImageView pImg = new ImageView(pimage);
-		pImg.setFitHeight(25);
-		pImg.setFitWidth(25);
+		FileInputStream prevFile = new FileInputStream("files\\Player\\ps.png");
+		Image prevImage = new Image(prevFile);
+		ImageView prevView = new ImageView(prevImage);
+		prevView.setFitHeight(25);
+		prevView.setFitWidth(25);
 
-		FileInputStream nimg = new FileInputStream("files\\Player\\ns.png");
-		Image nimage = new Image(nimg);
-		ImageView nImg = new ImageView(nimage);
-		nImg.setFitHeight(25);
-		nImg.setFitWidth(25);
-
-
-
-		Button pp = new Button("",Img);
-		Button ps = new Button("",pImg);
-		Button ns = new Button("",nImg);
-
-		pp.setOnAction((e) -> System.out.println("play/pause"));
-		pp.setTranslateX(-100);
-		pp.setTranslateY(100);
+		FileInputStream nextFile = new FileInputStream("files\\Player\\ns.png");
+		Image nextImage = new Image(nextFile);
+		ImageView nextView = new ImageView(nextImage);
+		nextView.setFitHeight(25);
+		nextView.setFitWidth(25);
 
 
 
-		ps.setOnAction((e) -> System.out.println("previous song"));
-		ps.setTranslateX(-150);
-		ps.setTranslateY(100);
+		Button playButton = new Button("",playView);
+		Button prevButton = new Button("",prevView);
+		Button nextButton = new Button("",nextView);
 
-		ns.setOnAction((e) -> System.out.println("next song"));
-		ns.setTranslateY(100);
-		ns.setTranslateX(-50);
+		
+		AtomicBoolean play = new AtomicBoolean(false);
+		playButton.setOnMouseClicked((e) -> {
+			playPause(play);
+			if (play.get()) playButton.setGraphic(playView);
+			else playButton.setGraphic(pauseView);
+			play.set(!play.get());
+			});
+		playButton.setTranslateX(-100);
+		playButton.setTranslateY(100);
 
-		Slider timesl = new Slider();
-		timesl.setMax(100);
-		timesl.setMin(0);
-		timesl.setTranslateY(50);
-		timesl.setMaxWidth(460);
+
+
+		prevButton.setOnMouseClicked((e) -> System.out.println("previous song"));
+		prevButton.setTranslateX(-150);
+		prevButton.setTranslateY(100);
+
+		nextButton.setOnMouseClicked((e) -> System.out.println("next song"));
+		nextButton.setTranslateY(100);
+		nextButton.setTranslateX(-50);
+
+		Slider timeSlider = new Slider();
+		timeSlider.setMax(100);
+		timeSlider.setMin(0);
+		timeSlider.setTranslateY(50);
+		timeSlider.setMaxWidth(460);
 		final Label timeLabel = new Label();
-		timesl.valueProperty().addListener(new ChangeListener<Number>() {
+		timeSlider.valueProperty().addListener(new ChangeListener<Number>() {
 
 			@Override
 			public void changed(
@@ -148,56 +167,86 @@ public class Root extends Application {
 
 
 		FileInputStream volumeFile = new FileInputStream("files\\Player\\volume.png");
-		FileInputStream muteFile = new FileInputStream("files\\Player\\mute.png");
 		Image volumeImage = new Image(volumeFile);
 		ImageView volumeView = new ImageView(volumeImage);
 		volumeView.setFitHeight(25);
 		volumeView.setFitWidth(25);
 
-		FileInputStream muteFIle = new FileInputStream("files\\Player\\mute.png");
-		Image muteImage = new Image(muteFIle);
+		FileInputStream muteFile = new FileInputStream("files\\Player\\mute.png");
+		Image muteImage = new Image(muteFile);
 		ImageView muteView = new ImageView(muteImage);
 		muteView.setFitHeight(25);
 		muteView.setFitWidth(25);
 
 		AtomicBoolean muted = new AtomicBoolean(false);
 		Button volumeButton = new Button("",volumeView);
-		volumeButton.setOnAction((e) -> {
-			muted.set(!muted.get());
+		volumeButton.setOnMouseClicked((e) -> {
 			volumeMute(muted);
 			if(muted.get()) {
-				volumeButton.setGraphic(muteView);
-			}
-			else {
 				volumeButton.setGraphic(volumeView);
 			}
+			else {
+				volumeButton.setGraphic(muteView);
+			}
+			muted.set(!muted.get());
 			
 		});
+		
+		
+		
 		
 		volumeButton.setTranslateX(100);
 		volumeButton.setTranslateY(100);
 
-		Slider vsl = new Slider();
-		vsl.setMax(30);
-		vsl.setMin(0);
-		vsl.setMaxWidth(80);
-		vsl.setTranslateX(165);
-		vsl.setTranslateY(100);
+		Slider volumeSlider = new Slider();
+		volumeSlider.setMax(30);
+		volumeSlider.setMin(0);
+		volumeSlider.setValue(15);
+		volumeSlider.setMaxWidth(80);
+		volumeSlider.setTranslateX(165);
+		volumeSlider.setTranslateY(100);
 
 
 		StackPane player = new StackPane();
-		player.getChildren().add(pp);
-		player.getChildren().add(ps);
-		player.getChildren().add(ns);
-		player.getChildren().add(timesl);
+		player.getChildren().add(playButton);
+		player.getChildren().add(prevButton);
+		player.getChildren().add(nextButton);
+		player.getChildren().add(timeSlider);
 		player.getChildren().add(timeLabel);
 		player.getChildren().add(volumeButton);
-		player.getChildren().add(vsl);
-
+		player.getChildren().add(volumeSlider);
 		player.setStyle("-fx-background-color: #FF0000");
+		
+		
+		HBox listsPane = new HBox();
+		listsPane.setAlignment(Pos.CENTER);
+		Button songsButton = new Button("songs");
+		Label songs_artistsLabel = new Label("  ");
+		Button artistsButton = new Button("artists");
+		Label artists_albumsLabel = new Label("  ");
+		Button albumsButton = new Button("albums");
+		listsPane.getChildren().add(songsButton);
+		listsPane.getChildren().add(songs_artistsLabel);
+		listsPane.getChildren().add(artistsButton);
+		listsPane.getChildren().add(artists_albumsLabel);
+		listsPane.getChildren().add(albumsButton);
+		
+		
+		VBox playlistsVbox = new VBox();
+		Hyperlink playlist1 = new Hyperlink("playlist 1");
+		Hyperlink playlist2 = new Hyperlink("playlist 2");
+		playlistsVbox.getChildren().add(playlist1);
+		playlistsVbox.getChildren().add(playlist2);
+		
+		
+		
 		root.add(player, 0, 2);
 		root.add(findHBox, 0, 0);
-		GridPane.setValignment(findHBox, VPos.CENTER);
+		root.add(listsPane, 1, 0);
+		root.add(playlistsVbox, 0, 1);
+		
+		
+		
 
 
 		Scene scene = new Scene(root, 650, 600);
@@ -207,6 +256,22 @@ public class Root extends Application {
 		primaryStage.setMinHeight(650);
 		primaryStage.show();
 
+		
+		scene.setOnKeyReleased((final KeyEvent KeyEvent) -> {
+			if (KeyEvent.getCode() == KeyCode.M) {
+				volumeMute(muted);
+				if(muted.get()) volumeButton.setGraphic(volumeView);
+				else volumeButton.setGraphic(muteView);
+				muted.set(!muted.get());
+				};
+			if (KeyEvent.getCode() == KeyCode.P) {
+				playPause(play);
+				if (play.get()) playButton.setGraphic(playView);
+				else playButton.setGraphic(pauseView);
+				play.set(!play.get());
+				};
+		});
+		
 
 	}
 
@@ -216,6 +281,13 @@ public class Root extends Application {
 
 
 	public static void find() {
+		
+	}
+	
+	
+	public static void playPause(AtomicBoolean play) {
+		if (play.get()) System.out.println("Shut up!");
+		else System.out.println("sing: che schifo i coefficienti di clebsch gordan");
 		
 	}
 
